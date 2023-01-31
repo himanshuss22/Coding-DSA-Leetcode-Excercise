9425d3496c266bd16aecce19779fc108e675ec1a
Himanshu Sharma <45357419+himanshuss22@users.noreply.github.com>
2023-01-30 17:24:31 -0800

    Update Sum of Two Integers.py

--- Python LeetCode Solutions/1. Sum of Two Integers/Sum of Two Integers.py
+++ Python LeetCode Solutions/1. Sum of Two Integers/Sum of Two Integers.py
@@ -1,11 +1,12 @@
-class Solution:
-    def getSum(self, a, b):
-        """
-        :type a: int
-        :type b: int
-        :rtype: int
-        """
-        mx, mask = 0x7FFFFFFF, 0xFFFFFFFF
-        while b:
-            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
-        return a if a <= mx else ~(a ^ mask)
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) 
+        {
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+            a = sum;
+            b = carry;
+        }
+        return a;
+    }
+}
